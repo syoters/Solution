bf6eb803145a940a08d18d241a6048b821ce078d
”SyoterS” <syoters@mail.ru>
2022-04-13 10:33:06 +0300

    сравнение бинарных деревьев

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -31,77 +31,43 @@ public class Main {
     }
 
     private void run() throws IOException {
-        int a[], b[], k = 13;
-        try (Scanner in = new Scanner(new FileInputStream("test.txt"))) {
-//        try (Scanner in = new Scanner(System.in)) {
-            a = Arrays.stream(in.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            b = Arrays.stream(in.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        }
-//        try (BufferedReader bf = new BufferedReader(new FileReader("test.txt"))) {
-//            a = Arrays.stream(bf.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-//            b = Arrays.stream(bf.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-//        }
-        for (int i = 0; i < 15; i++) {
-            System.out.print(find1(i, a, 1, a.length - 1) + " ");
-        }
-        System.out.println();
-        System.out.println(Arrays.toString(a));
-        System.out.println();
+        TreeNode p = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        TreeNode q = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        TreeNode z = new TreeNode(1, new TreeNode(2), new TreeNode());
+        TreeNode z1 = new TreeNode(1, new TreeNode(), new TreeNode(2));
 
-        int p[];
-        p = findSum(17, a, 1, a.length - 1);
-        System.out.println(Arrays.toString(p) + "\t" + a[p[0]] + " " + a[p[1]]);
-        p = findSumBin(17, a, 1, a.length - 1);
-        System.out.println(Arrays.toString(p) + "\t" + a[p[0]] + " " + a[p[1]]);
-
+        System.out.println(isSameTree(p, q));
+        System.out.println(isSameTree(p, z));
+        System.out.println(isSameTree(z, z1));
     }
 
-    private int[] findSum(int value, int[] sortArray, int left, int right) {
-        while (right > left) {
-            int sum = sortArray[left] + sortArray[right];
-//            System.out.println(left + "\t" + right + "\t" + sum);  // отладка
-            if (sum == value) return new int[]{left, right};
-            if (sum < value) left++;
-            if (sum > value) right--;
-        }
-        return new int[]{-1, -1};
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null ^ q == null) return false;
+        return p == q || p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 
-    private int[] findSumBin(int value, int[] sortArray, int left, int right) {
-        while (right > left) {
-            int sum = sortArray[left] + sortArray[right];
-            System.out.println("[" + left + " " + right + "]\t" + sortArray[left] + " " + sortArray[right] + "\t" + sum);  // отладка
-            if (sum == value) return new int[]{left, right};
-            if (sum < value) left = (left + right) / 2;
-            if (sum > value) right = (left + right) / 2;
+
+    // Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) {this.val = val;}
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
         }
-        return new int[]{-1, -1};
-    }
 
-    private int find1(int value, int[] array, int left, int right) {
-        while (right >= left) {
-            int midpoint = (left + right) / 2;
-            if (value == array[midpoint]) return midpoint;
-
-            if (array[midpoint] > value) right = midpoint - 1;
-            if (array[midpoint] < value) left = midpoint + 1;
+        @Override
+        public String toString() {
+            return String.format("[%d, %d, %d]", val, left.val, right.val);
         }
-        return -1;
-    }
-
-    private int find(int value, int[] array, int left, int right) {
-        if (right < left) return -1;
-        int midpoint = (left + right) / 2;
-        if (value == array[midpoint]) return midpoint;
-
-        if (array[midpoint] > value) return find(value, array, left, midpoint - 1);
-        if (array[midpoint] < value) return find(value, array, midpoint + 1, right);
-
-        return -1;
     }
 
 }
-
-
-
 
