5d3de82f58f1382dc3e3c756768d3a91d57568fa
”SyoterS” <syoters@mail.ru>
2022-04-18 13:47:13 +0300

    Ищем сколько пингов было за последине 3000мс поиск вхождения по скользящему окну

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -27,11 +27,26 @@ public class Main {
     public static void main(String[] args) throws IOException {
         long t = new Date().getTime();
 //        new Main().run();
-        System.out.println(new Solution().isSubsequence("abc", "ahbgdc"));
-        System.out.println(new Solution().isSubsequence("axc", "ahbgdc"));
-        System.out.println(new Solution().isSubsequence("b", "abc"));
-        System.out.println(new Solution().isSubsequence("rjufvjafbxnbgriwgokdgqdqewn", "mjmqqjrmzkvhxlyruonekhhofpzzslupzojfuoztvzmmqvmlhgqxehojfowtrinbatjujaxekbcydldglkbxsqbbnrkhfdnpfbuaktupfftiljwpgglkjqunvithzlzpgikixqeuimmtbiskemplcvljqgvlzvnqxgedxqnznddkiujwhdefziydtquoudzxstpjjitmiimbjfgfjikkjycwgnpdxpeppsturjwkgnifinccvqzwlbmgpdaodzptyrjjkbqmgdrftfbwgimsmjpknuqtijrsnwvtytqqvookinzmkkkrkgwafohflvuedssukjgipgmypakhlckvizmqvycvbxhlljzejcaijqnfgobuhuiahtmxfzoplmmjfxtggwwxliplntkfuxjcnzcqsaagahbbneugiocexcfpszzomumfqpaiydssmihdoewahoswhlnpctjmkyufsvjlrflfiktndubnymenlmpyrhjxfdcq"));
+
+        RecentCounter recentCounter = new RecentCounter();
+        System.out.println(recentCounter.ping(1));
+        System.out.println(recentCounter.ping(100));
+        System.out.println(recentCounter.ping(3001));
+        System.out.println(recentCounter.ping(3002));
+        System.out.println(recentCounter.requests);
+
         System.out.println(new Date().getTime() - t + " ms");
+    }
+
+    private int find(int value, int[] array, int left, int right) {
+        int midpoint = 0;
+        while (right >= left) {
+            midpoint = (left + right) / 2;
+            if (value == array[midpoint]) return midpoint;
+            if (array[midpoint] > value) right = midpoint - 1;
+            if (array[midpoint] < value) left = midpoint + 1;
+        }
+        return left;
     }
 
     private void run() throws IOException {
@@ -40,28 +55,59 @@ public class Main {
 
 }
 
-class Solution {
-    public boolean isSubsequence(String s, String t) {
-        if (s.isEmpty()) return true;
-        int is = 0;
-        for (int it = 0; it < t.length(); it++) {
-            if (s.charAt(is) == t.charAt(it)) is++;
-            if (is == s.length()) return true;
-        }
+class RecentCounter {
+    int count = 0;
+    ArrayList<Integer> requests = new ArrayList<>();
+    Queue<Integer> q = new LinkedList<>();
 
-        return false;
+    public RecentCounter() {}
+
+    public int ping(int t) {
+        requests.add(t);
+        return requests.size() - find(t - 3000, requests, 0, requests.size() - 1);
     }
 
+    // с запоминанием последней позиции, самый быстрый способ
+    public int ping_lifeHack(int t) {
+        while (count < requests.size() && requests.get(count) < t - 3000) count++;
+        requests.add(t);
+        return requests.size() - count;
+    }
 
-    public boolean isSubsequence1(String s, String t) {
-        StringBuilder sb = new StringBuilder();
-//        sb.append("^");
-        for (int i = 0; i < s.length(); i++) {
-            sb.append(".*").append(s.charAt(i)).append("+");
+    // поиск через очередь(выпихиваем все элементы что не входят в наш интервал)
+    public int ping_Queue(int t) {
+        int rem = t - 3000;
+        q.add(t);
+        while (q.peek() < rem) q.remove();
+        return q.size();
+    }
+
+    //линейный перебор всего/ скользящее окно
+    public int ping_line(int t) {
+        requests.add(t);
+
+        int count = 0;
+        for (int i = requests.size() - 1; i >= 0; i--) {
+            if (requests.get(i) >= t - 3000 && requests.get(i) <= t) count++;
         }
-        sb.append(".*");
+        return count;
+    }
 
-        return t.matches(sb.toString());
+    // нечеткий поиск, возвращает индекс где ближе всего искомое значение(left right можно поигратся)
+    private int find(int value, ArrayList<Integer> array, int left, int right) {
+        while (right >= left) {
+            int midpoint = (left + right) / 2;
+            if (array.get(midpoint) == value) return midpoint;
+            if (array.get(midpoint) > value) right = midpoint - 1;
+            if (array.get(midpoint) < value) left = midpoint + 1;
+        }
+        return left;
     }
 }
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
 
