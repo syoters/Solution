53748c341ca68990480079e39980b467017c2f1c
”SyoterS” <syoters@mail.ru>
2022-04-25 15:23:55 +0300

    1837 . Сумма цифр в базе K. Конвертация 10числа в базу K.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -28,20 +28,12 @@ public class Main {
         long t = new Date().getTime();
 //        new Main().run();
         Solution solution = new Solution();
-//        System.out.println(solution.totalHammingDistanceTest(new int[]{4, 14, 2}));
-//        System.out.println(solution.totalHammingDistance(new int[]{4, 14, 4}));
-        System.out.println(solution.totalHammingDistanceTest(new int[]{6, 1, 8, 6, 8}));
-        System.out.println("------");
-        System.out.println(solution.totalHammingDistance0(new int[]{6, 1, 8, 6, 8}));
+        System.out.println(solution.decToBaseK(34, 6));
+        System.out.println(solution.sumBase(34, 6));
 
-//        Scanner file = new Scanner(new File("test.txt"));
-//        String Snums[] = file.nextLine().split(",");
-//        int nums[] = new int[Snums.length];
-//        for (int i = 0; i < Snums.length; i++) {
-//            nums[i] = Integer.parseInt(Snums[i]);
-//        }
-//        System.out.println(solution.totalHammingDistance(nums));
-//        System.out.println(solution.totalHammingDistance0(nums)); //вот тут мой код запарывается по времени
+        System.out.println(solution.decToBaseK(10, 10));
+        System.out.println(solution.sumBase(10, 10));
+
         System.out.println(new Date().getTime() - t + " ms");
     }
 
@@ -52,74 +44,14 @@ public class Main {
 }
 
 class Solution {
-    /*
-        477 . Общее расстояние Хэмминга
-        Расстояние Хэмминга между двумя целыми числами — это количество позиций,
-        в которых соответствующие биты различны.
-        Учитывая целочисленный массив nums, вернуть сумму расстояний Хэмминга
-        между всеми парами целых чисел в nums
-    */
-
-    // мой код считает, но долго
-    public int totalHammingDistance(int[] nums) {
-        int sum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            for (int i1 = i + 1; i1 < nums.length; i1++) {
-                sum += HammingDistance(nums[i], nums[i1]);
-            }
-        }
-        return sum;
+    public int sumBase(int n, int k) {
+        return (n / k > 0 ? sumBase(n / k, k) : 0) + (n % k);
     }
 
-    private int HammingDistance(int a, int b) {
-        int n = a ^ b;
-        int sum = 0;
-        while (n > 0) {
-            if ((n & 1) == 1) sum++;  //if (n % 2 == 1)
-            n >>= 1;
-        }
-        return sum;
-    }
-
-    //код из подсказок
-    public int totalHammingDistance0(int[] nums) {
-        long mask = 1L << 31;
-        int totalans = 0;
-        while (mask != 0) {
-            int count = 0;
-            for (int i = 0; i < nums.length; i++) {
-                if ((mask & nums[i]) >= 1) count++;
-            }
-            mask = mask >> 1;
-            //количество чисел с установленным данным битом * количество чисел с неустановленным данным битом
-            totalans = totalans + ((nums.length - count) * (count));
-        }
-        return totalans;
-    }
-
-    int totalHammingDistanceTest(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            System.out.println(getBin(nums[i], 4));
-        }
-        System.out.println();
-        int sum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            for (int i1 = i + 1; i1 < nums.length; i1++) {
-                int n = nums[i] ^ nums[i1];
-                System.out.print(getBin(n, 4));
-                System.out.print("\t" + nums[i] + " vs " + nums[i1]);
-                System.out.println("\t" + HammingDistance(nums[i], nums[i1]));
-                sum += HammingDistance(nums[i], nums[i1]);
-            }
-        }
-        System.out.println();
-
-        return sum;
-    }
-
-    private String getBin(int num, int digit) {
-        return String.format("%" + digit + "s", Integer.toString(num, 2))
-                .replaceAll(" ", "0");
+    StringBuilder decToBaseK(int n, int k) {
+        return new StringBuilder()
+                .append(n / k > 0 ? decToBaseK(n / k, k) : "")
+                .append(n % k);
     }
 }
 
